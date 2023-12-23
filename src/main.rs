a1a237521a288ff27892d0f26848fd4ac6f1a7dd
Tiramister <nh111424@gmail.com>
2023-12-23 15:20:09 +0900

    feat: リクエストを飛ばしてみる

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let response = reqwest::get("https://cafe.kiite.jp/api/cafe/next_song")
+        .await?
+        .text()
+        .await?;
+    println!("{}", response);
+    Ok(())
 }
