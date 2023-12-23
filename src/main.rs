9851e889c5d6f579258a3bda4dfa163b73189c5a
Tiramister <nh111424@gmail.com>
2023-12-23 15:33:52 +0900

    feat: レスポンスをパースする

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct NextSongResponse {
+    id: u32,
+    video_id: String,
+    title: String,
+    artist_id: u32,
+    artist_name: String,
+    start_time: String,
+    msec_duration: u32,
+    thumbnail: String,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let response = reqwest::get("https://cafe.kiite.jp/api/cafe/next_song")
         .await?
-        .text()
+        .json::<NextSongResponse>()
         .await?;
-    println!("{}", response);
+    eprintln!("{:?}", response);
     Ok(())
 }
