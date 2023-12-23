f8203e3820fec07df8b68662621275cf070e2690
Tiramister <nh111424@gmail.com>
2023-12-23 19:13:27 +0900

    feat: 通知対象となる曲を指定できるように

--- README.md
+++ README.md
@@ -5,6 +5,14 @@
 Slack の通知先への Webhook URL を環境変数に設定する。
 
 ```shell
-export WEBHOOK_URL=https://hooks.slack.com/services/...
+$ export WEBHOOK_URL=https://hooks.slack.com/services/...
+```
+
+通知対象とする曲は、`videos.txt` に行区切りで指定する（ゆくゆくは自動取得にしたい）。
+
+```shell
+$ read LIST_URL; curl "https://cafe.kiite.jp/api/playlists/contents/detail?list_id=$(basename $LIST_URL)" | jq -r '.songs |  .[].video_id' >> videos.txt
+# 入力待ちになるので、Kiite プレイリストの URL を入力する
+https://kiite.jp/playlist/XXXXXXXX
 ```
 
--- src/main.rs
+++ src/main.rs
@@ -30,10 +30,21 @@ async fn main() -> anyhow::Result<()> {
         .await?;
     eprintln!("{:?}", response);
 
+    let objective_video_ids = std::fs::read_to_string("videos.txt")?;
+    if !objective_video_ids
+        .lines()
+        .any(|video_id| video_id == &response.video_id)
+    {
+        eprintln!("Skip Slack notification");
+        return Ok(());
+    }
+
     let slack_body = SlackBody {
         text: format!("{} {}", response.title, response.thumbnail),
     };
     let webhook_url = env::var("WEBHOOK_URL")?;
     client.post(webhook_url).json(&slack_body).send().await?;
+    eprintln!("Sent Slack notification");
+
     Ok(())
 }
